b4d4ff8c491f6afe77cb969c44bc840d8ba98f40
Toolsforall <76138254+toolsforall@users.noreply.github.com>
2021-01-07 07:33:45 +0530

    Add files via upload

---
+++ ShowDayTime/README.md
@@ -0,0 +1,1 @@
+# show-day-of-the-week
---
+++ ShowDayTime/main.js
@@ -0,0 +1,32 @@
+let output = document.getElementById('output');
+let numberDate = document.getElementById('date');
+
+
+let d = new Date();
+let day = d.getDay();
+let ndate = d.toLocaleString();
+
+switch (day){
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5: 
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+output.innerHTML = day;
+date.innerHTML = ndate;
